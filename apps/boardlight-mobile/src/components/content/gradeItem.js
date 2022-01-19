c49a3a5c75a45c08e27046f4e8db26a6240882bb
Xeretis <70800211+Xeretis@users.noreply.github.com>
2022-01-19 16:57:59 +0100

    Made a grade component

---
+++ apps/boardlight-mobile/src/components/content/gradeItem.js
@@ -0,0 +1,51 @@
+import { Avatar, List, useTheme } from "react-native-paper";
+
+import React from "react";
+
+export const GradeItem = (props) => {
+  const { grade } = props.data;
+  const theme = useTheme();
+
+  const colors = {
+    1: "#f44336",
+    // orange:
+    2: "#ff9800",
+    // yellow
+    3: "#ffeb3b",
+    // light green
+    4: "#8bc34a",
+    // dark green
+    5: "#4caf50",
+  };
+
+  // TODO: On click bottom sheet
+
+  return (
+    <List.Item
+      title={props.data.type}
+      description={props.data.name}
+      left={(props) => (
+        <Avatar.Text
+          {...props}
+          style={{ backgroundColor: colors[grade], margin: 8 }}
+          size={40}
+          color="#000000"
+          label={grade}
+        />
+      )}
+      style={{
+        padding: 0,
+        backgroundColor: theme.colors.surface,
+        borderRadius: theme.roundness,
+        elevation: 1,
+        shadowRadius: 0.75,
+        shadowOpacity: 0.24,
+        shadowOffset: {
+          width: 0,
+          height: 0.75,
+        },
+        height: 56,
+      }}
+    />
+  );
+};
