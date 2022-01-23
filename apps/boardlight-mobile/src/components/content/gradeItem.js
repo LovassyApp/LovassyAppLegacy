f6bef9b39f9af46aac9d55ace8523353fa20b308
Xeretis <70800211+Xeretis@users.noreply.github.com>
2022-01-23 21:37:25 +0100

    Made a pretty sexy bottom sheet dingus for the grades

--- apps/boardlight-mobile/src/components/content/gradeItem.js
+++ apps/boardlight-mobile/src/components/content/gradeItem.js
@@ -1,6 +1,8 @@
-import { Avatar, List, useTheme } from "react-native-paper";
+import { Avatar, Caption, List, Subheading, Text, Title, useTheme } from "react-native-paper";
+import React, { useCallback, useMemo, useRef } from "react";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
 
-import React from "react";
+import BottomSheet from "react-native-gesture-bottom-sheet";
 import { lightTheme } from "../../utils/theme/themes";
 import { useSelector } from "react-redux";
 
@@ -21,35 +23,102 @@ export const GradeItem = (props) => {
     5: "#4caf50",
   };
 
-  // TODO: On click bottom sheet
+  const bottomSheetRef = useRef();
+
+  const styles = StyleSheet.create({
+    sheetContainer: {
+      flex: 1,
+      padding: 20,
+    },
+    dataContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    title: {
+      paddingTop: 8,
+    },
+    subTitle: {
+      paddingBottom: 8,
+    },
+  });
 
   return (
-    <List.Item
-      title={props.data.type}
-      description={props.data.name}
-      left={(props) => (
-        <Avatar.Text
-          {...props}
-          style={{ backgroundColor: colors[grade], margin: 8 }}
-          size={40}
-          color="#000000"
-          label={grade}
-        />
-      )}
-      style={{
-        padding: 0,
-        // Because of adaptive mode in the default dark theme of react native paper
-        backgroundColor: reduxTheme === lightTheme ? theme.colors.surface : "#1e1e1e",
-        borderRadius: theme.roundness,
-        elevation: 1,
-        shadowRadius: 0.75,
-        shadowOpacity: 0.24,
-        shadowOffset: {
-          width: 0,
-          height: 0.75,
-        },
-        height: 56,
-      }}
-    />
+    <>
+      <BottomSheet
+        backgroundColor={reduxTheme === lightTheme ? "#25252599" : "#00000099"}
+        sheetBackgroundColor={reduxTheme === lightTheme ? theme.colors.surface : "#1e1e1e"}
+        radius={theme.roundness}
+        ref={bottomSheetRef}
+        height={220}>
+        <View style={styles.sheetContainer}>
+          <View style={styles.dataContainer}>
+            <View>
+              <Title style={styles.title}>{props.data.type}</Title>
+              <Text style={styles.subTitle}>{props.data.name}</Text>
+            </View>
+            <Avatar.Text
+              {...props}
+              style={{ backgroundColor: colors[grade], margin: 8 }}
+              size={56}
+              color="#000000"
+              label={grade}
+            />
+          </View>
+          <View style={{ ...styles.dataContainer, paddingTop: 4 }}>
+            <Text>Subject:</Text>
+            <Text>{props.data.subject}</Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text>Teacher:</Text>
+            <Text>{props.data.teacher}</Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text>Date:</Text>
+            <Text>
+              {
+                // Budget inline formatting let's go
+                props.data.date
+                  .replace("-", ". ")
+                  .replace("-", ". ")
+                  .replace("T", ". ")
+                  .replace("Z", "")
+              }
+            </Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text>Weight:</Text>
+            <Text>{props.data.weight}%</Text>
+          </View>
+        </View>
+      </BottomSheet>
+      <List.Item
+        title={props.data.type}
+        description={props.data.name}
+        onPress={() => bottomSheetRef.current.show()}
+        left={(props) => (
+          <Avatar.Text
+            {...props}
+            style={{ backgroundColor: colors[grade], margin: 8 }}
+            size={40}
+            color="#000000"
+            label={grade}
+          />
+        )}
+        style={{
+          padding: 0,
+          // Because of adaptive mode in the default dark theme of react native paper
+          backgroundColor: reduxTheme === lightTheme ? theme.colors.surface : "#1e1e1e",
+          borderRadius: theme.roundness,
+          elevation: 1,
+          shadowRadius: 0.75,
+          shadowOpacity: 0.24,
+          shadowOffset: {
+            width: 0,
+            height: 0.75,
+          },
+          height: 56,
+        }}
+      />
+    </>
   );
 };
