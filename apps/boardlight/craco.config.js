ab8da141d6daa2e2b48c58f1e8aa1f8722246fa3
Máté Gyimesi <gyim.mate@gmail.com>
2022-01-23 02:27:30 +0100

    Boardlight cleanup, hot reloads finally work now

--- apps/boardlight/craco.config.js
+++ apps/boardlight/craco.config.js
@@ -1,27 +1,23 @@
-const path = require("path");
-const { getLoader, loaderByName } = require("@craco/craco");
+const path = require('path');
+const { getLoader, loaderByName } = require('@craco/craco');
+const webpack = require('webpack');
 
 //add all packages from the monorepo here as well, or they won't be compiled
 const packages = [];
-packages.push(path.join(__dirname, "../../packages/blueboard-client"));
-packages.push(path.join(__dirname, "../../packages/blueboard-client-react"));
+packages.push(path.join(__dirname, '../../packages/blueboard-client'));
+packages.push(path.join(__dirname, '../../packages/blueboard-client-react'));
 
 module.exports = {
-  webpack: {
-    configure: (webpackConfig, arg) => {
-      const { isFound, match } = getLoader(
-        webpackConfig,
-        loaderByName("babel-loader")
-      );
-      if (isFound) {
-        const include = Array.isArray(match.loader.include)
-          ? match.loader.include
-          : [match.loader.include];
+    webpack: {
+        configure: (webpackConfig, arg) => {
+            const { isFound, match } = getLoader(webpackConfig, loaderByName('babel-loader'));
+            if (isFound) {
+                const include = Array.isArray(match.loader.include) ? match.loader.include : [match.loader.include];
 
-        match.loader.include = include.concat(packages);
-      }
+                match.loader.include = include.concat(packages);
+            }
 
-      return webpackConfig;
+            return webpackConfig;
+        },
     },
-  },
 };
