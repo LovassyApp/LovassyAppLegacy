a426acc930694ce4f0096290867840d23eb3bac4
Xeretis <70800211+Xeretis@users.noreply.github.com>
2022-02-08 10:29:04 +0100

    Fixed something I mixed

--- apps/boardlight-mobile/src/components/content/gradeItem.js
+++ apps/boardlight-mobile/src/components/content/gradeItem.js
@@ -10,7 +10,7 @@ import { useSelector } from "react-redux";
 export const GradeItem = (props) => {
   const { grade } = props.data;
   const theme = useTheme();
-  const reduxTheme = useSelector((state) => state.theme.value);
+  const reduxTheme = useSelector((state) => state.settings.state);
 
   const colors = {
     1: "#f44336",
