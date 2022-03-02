b0c2e01eecbed33ac75d6017fe7ad26a40975f10
Máté Gyimesi <gyim.mate@gmail.com>
2022-03-02 09:26:39 +0100

    Made conditional eager loading in boardlight-mobile

--- apps/boardlight-mobile/src/utils/api/apiUtils.js
+++ apps/boardlight-mobile/src/utils/api/apiUtils.js
@@ -2,90 +2,105 @@ import store from "../../store/store";
 
 export const fetchLolo = async (client, refresh = true, token = null) => {
   const { dispatch } = store;
+  const state = store.getState();
 
-  console.log("DEBUG: Fetching lolo...");
+  if (state.control.value.permissions.includes("General.lolo")) {
+    console.log("DEBUG: Fetching lolo...");
 
-  try {
-    const res = await client.lolo.get(refresh, token);
+    try {
+      const res = await client.lolo.get(refresh, token);
 
-    dispatch({
-      type: "coins/setCoins",
-      payload: res.coins,
-    });
+      dispatch({
+        type: "coins/setCoins",
+        payload: res.coins,
+      });
 
-    dispatch({
-      type: "control/setUserBalance",
-      payload: res.balance,
-    });
-  } catch (err) {
-    console.log(err);
+      dispatch({
+        type: "control/setUserBalance",
+        payload: res.balance,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
 export const fetchGrades = async (client, refresh = true, token = null) => {
   const { dispatch } = store;
+  const state = store.getState();
 
-  console.log("DEBUG: Fetching grades...");
+  if (state.control.value.permissions.includes("General.grades")) {
+    console.log("DEBUG: Fetching grades...");
 
-  try {
-    const res = await client.kreta.grades(refresh, token);
+    try {
+      const res = await client.kreta.grades(refresh, token);
 
-    dispatch({
-      type: "kreta/setGradeValue",
-      payload: res,
-    });
-  } catch (err) {
-    console.log(err);
+      dispatch({
+        type: "kreta/setGradeValue",
+        payload: res,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
 export const fetchStore = async (client, token = null) => {
   const { dispatch } = store;
+  const state = store.getState();
 
-  console.log("DEBUG: Fetching store...");
+  if (state.control.value.permissions.includes("Store.view")) {
+    console.log("DEBUG: Fetching store...");
 
-  try {
-    const res = await client.store.all(token);
+    try {
+      const res = await client.store.all(token);
 
-    dispatch({
-      type: "store/setStore",
-      payload: res,
-    });
-  } catch (err) {
-    console.log(err);
+      dispatch({
+        type: "store/setStore",
+        payload: res,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
 export const fetchInventory = async (client, token = null) => {
   const { dispatch } = store;
+  const state = store.getState();
 
-  console.log("DEBUG: Fetching inventory...");
+  if (state.control.value.permissions.includes("Inventory.view")) {
+    console.log("DEBUG: Fetching inventory...");
 
-  try {
-    const res = await client.inventory.items(token);
+    try {
+      const res = await client.inventory.items(token);
 
-    dispatch({
-      type: "inventory/setInventory",
-      payload: res,
-    });
-  } catch (err) {
-    console.log(err);
+      dispatch({
+        type: "inventory/setInventory",
+        payload: res,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
 export const fetchRequests = async (client, token = null) => {
   const { dispatch } = store;
+  const state = store.getState();
 
-  console.log("DEBUG: Fetching requests...");
+  if (state.control.value.permissions.includes("Requests.view")) {
+    console.log("DEBUG: Fetching requests...");
 
-  try {
-    const res = await client.lolo_request.get(token);
+    try {
+      const res = await client.lolo_request.get(token);
 
-    dispatch({
-      type: "requests/setRequests",
-      payload: res,
-    });
-  } catch (err) {
-    console.log(err);
+      dispatch({
+        type: "requests/setRequests",
+        payload: res,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
